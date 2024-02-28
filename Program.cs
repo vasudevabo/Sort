ca8a5c89db43dd5b18235255dc718164dd270aad
Vasudeva Reddy Bolleddula <vb1@usf.edu>
2024-02-28 11:09:13 -0500

    used tuple to swap

--- Program.cs
+++ Program.cs
@@ -24,9 +24,7 @@ class BubbleSort
             {
                 if (arr[j] > arr[j + 1])
                 {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]);
                 }
             }
         }
